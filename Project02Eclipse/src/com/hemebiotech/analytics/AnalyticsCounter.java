ab72764f1d7ef39e78ec9cf86bb7edd1604f326f
Alexandra <a.craipain@yahoo.fr>
2022-05-24 09:46:23 +0200

    update fix bug

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,74 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+	/**
+	 * Analyzes the collected data
+	 * 
+	 */
+
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
+		/**
+		 * read File text and analyze a data
+		 * 
+		 * create a new file with analyze
+		 */
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+			
+		ReadSymptomDataFromFile lecture = new ReadSymptomDataFromFile(
+				"D:\\sauvegardeGit\\Projet-2-biotech\\Project02Eclipse\\src\\symptoms.txt");
+		
+		
+		
+		List<String> symptomList = lecture.GetSymptoms();
+		Map<String, Integer> resultat = new HashMap<String, Integer>();
+
+		resultat = countSymptom(symptomList);
+		afficheMap(resultat);
+		WriterDataInFile writeFile = new WriterDataInFile(
+				"D:\\sauvegardeGit\\Projet-2-biotech\\Project02Eclipse\\src\\result.out");
+		writeFile.SetData(resultat);
+
 	}
+
+	public static Map<String, Integer> countSymptom(List<String> symptom) {
+		/**
+		 * Create Map order with all symptom and this occurence
+		 * 
+		 * @return ordered map : Key is the symptom and Value is symptom count
+		 */
+
+		Map<String, Integer> symptomNombre = new TreeMap<String, Integer>();
+
+		for (String s : symptom) {
+
+			if (!symptomNombre.containsKey(s)) {
+				symptomNombre.put(s, 1);
+			} else {
+				Integer value = symptomNombre.get(s) + 1;
+				symptomNombre.replace(s, value);
+			}
+		}
+		return symptomNombre;
+
+	}
+
+	public static void afficheMap(Map<String, Integer> map) {
+
+		map.forEach((symptom, compteur) -> System.out.println(symptom + " " + compteur));
+
+	}
+
 }
